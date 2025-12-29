f5ef9fb6d80bc2f7d9b3cb5bbc07ac1cbcf20364
soumyashreechoudhury <soumyashreechoudhury01@gmail.com>
2025-12-29 12:44:12 +0530

    Add files via upload

---
+++ Assignment 3/Assignment3_1.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class Assignment3_1 {
+	public static void main(String[] args) {
+		int age=22;
+		System.out.println("Age:"+ age);
+		
+		float height=170.5F;
+		System.out.println("Height:"+ height);
+		
+		long mobileNumber=9876543210L;
+		System.out.println("Mobile number:"+ mobileNumber);
+		
+		boolean isStudent=true;
+		System.out.println("Is Student:"+ isStudent);
+
+}
+}
