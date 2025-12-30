20101be49fcd2ab95fbb65f74d65d299eb6f11d3
soumyashreechoudhury <soumyashreechoudhury01@gmail.com>
2025-12-30 19:14:14 +0530

    Add files via upload

---
+++ GrowskillIT/src/day1/Program2_Variables.java
@@ -0,0 +1,10 @@
+package day1;
+public class Program2_Variables {
+	public static void main(String[] args) {
+		int num=2147483647;
+		System.out.println(num);
+		long phoneNumber=9876543210000L;
+		System.out.println(phoneNumber);
+	}
+
+}
---
+++ GrowskillIT/src/day1/Program3_DatatypeExamples.java
@@ -0,0 +1,41 @@
+package day1;
+public class Program3_DatatypeExamples {
+	public static void main(String[] args) {
+		boolean result=true;
+		System.out.println(result);
+		
+		byte ans=127;
+		System.out.println(ans);
+		
+		short data=-32000;
+		System.out.println(data);
+		
+		char alpha='A';
+		System.out.println(alpha);	
+		
+		float marks1=98.578F;
+		System.out.println(marks1);
+		
+		float marks2=98.578123F;
+		System.out.println(marks2);
+		
+		float marks3=9857.81231234F;
+		System.out.println(marks3);
+		
+		float marks4=9.85781232345F;
+		System.out.println(marks4);
+				
+		double data3=9.12345678;
+		System.out.println(data3);
+		double data4=9123.45678;
+		System.out.println(data4);
+		double data5=9.1234567834535435323;
+		System.out.println(data5);
+		
+		
+		
+		
+	}
+	
+
+}
